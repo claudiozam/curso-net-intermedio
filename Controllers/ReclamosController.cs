5b47c9c7f2a70ab2db54302b2fbc99e4e74e2646
claudio z <claudio.zamoszczyk@gmail.com>
2023-08-15 17:19:08 -0300

    commit inicial

---
+++ Controllers/ReclamosController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationSistemaReclamosV2.Models.ViewModels;
+
+namespace WebApplicationSistemaReclamosV2.Controllers
+{
+    public class ReclamosController : Controller
+    {
+        // GET: ReclamosController1cs
+        public ActionResult Index()
+        {
+            List<ReclamoViewModel> listReclamos = new List<ReclamoViewModel>();
+            listReclamos.Add(new ReclamoViewModel(1, "Ejemplo1", "Desc1", DateTime.Now, "nuevo"));
+            listReclamos.Add(new ReclamoViewModel(2, "Ejemplo2", "Desc2", DateTime.Now, "nuevo"));
+            listReclamos.Add(new ReclamoViewModel(3, "Ejemplo3", "Desc3", DateTime.Now, "nuevo"));
+
+            return View(listReclamos);
+        }
+
+        // GET: ReclamosController1cs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ReclamosController1cs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ReclamosController1cs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReclamosController1cs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ReclamosController1cs/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ReclamosController1cs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/ViewModels/ReclamoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationSistemaReclamosV2.Models.ViewModels
+{
+    public class ReclamoViewModel
+    {
+
+        private long id;
+        private string titulo;
+        private string descripcion;
+        private DateTime fechaAlta;
+        private string estado;
+
+        public ReclamoViewModel() { }
+
+        public ReclamoViewModel(long id, string titulo, string descripcion, DateTime fechaAlta, string estado)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.descripcion = descripcion;
+            this.fechaAlta = fechaAlta;
+            this.estado = estado;
+        }
+
+        [Display(Name = "ID")]
+        public long Id { get => id; set => id = value; }
+
+        [Required(ErrorMessage = "Falta completar el titulo")]
+        [StringLength(50)]
+        [Display(Name = "Titulo")]
+        public string Titulo { get => titulo; set => titulo = value; }
+
+        [Required(ErrorMessage = "Falta completar la descripcion")]
+        [Display(Name = "Descripcion completa del reclamo")]
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+
+        [Display(Name = "Fecha de alta")]
+        public DateTime FechaAlta { get => fechaAlta; set => fechaAlta = value; }
+
+        [Display(Name = "Estado actual")]
+        public string Estado { get => estado; set => estado = value; }
+    }
+}
